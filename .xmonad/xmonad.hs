2b64007b7653a7f03573e01bd305b0b7e6722c76
Daniel Fullmer <dan.fullmer@printfulfillmentservices.com>
2012-05-09 13:51:49 -0400

    Set up xmobar and modified xmonad.hs

--- .xmonad/xmonad.hs
+++ .xmonad/xmonad.hs
@@ -1,9 +1,32 @@
-import XMonad
-import XMonad.Config.Gnome
-import XMonad.Hooks.ManageHelpers
+import System.IO
 
-main = xmonad $ gnomeConfig {
-    --modMask = mod4Mask,
-    --layoutHook = smartBorders ()
-    manageHook = composeOne [ isFullscreen -?> doFullFloat ] <+> manageHook gnomeConfig
-}
+import XMonad
+import XMonad.Hooks.FadeInactive
+import XMonad.Hooks.ManageDocks
+import XMonad.Hooks.ManageHelpers
+import XMonad.Hooks.DynamicLog
+import XMonad.Layout.NoBorders
+import XMonad.Layout.Tabbed
+import XMonad.Util.Run (spawnPipe)
+
+main = xmonad =<< statusBar myBar myPP toggleStrutsKey myConfig
+
+-- Command to launch the bar.
+myBar = "xmobar -x 1"
+
+-- Custom PP, configure it as you like. It determines what is being written to the bar.
+myPP = xmobarPP { ppCurrent = xmobarColor "#429942" "" . wrap "<" ">" }
+
+-- Key binding to toggle the gap for the bar.
+toggleStrutsKey XConfig {XMonad.modMask = modMask} = (modMask, xK_b)
+
+-- Configuration
+myConfig = defaultConfig
+        { terminal = "gnome-terminal"
+        , manageHook = manageDocks <+> manageHook defaultConfig
+        , layoutHook = noBorders $
+            Tall 1 (3/100) (1/2) |||
+            Mirror (Tall 1 (3/100) (1/2)) |||
+            Full
+        , logHook = fadeInactiveLogHook 0.9
+        }
