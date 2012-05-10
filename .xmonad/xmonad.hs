0c98ae17ee412ce4849a8a51a787e5b1a473ddea
Daniel Fullmer <daniel.fullmer@byu.edu>
2012-05-09 23:08:15 -0600

    Added floats in xmonad

--- .xmonad/xmonad.hs
+++ .xmonad/xmonad.hs
@@ -9,8 +9,6 @@ import XMonad.Layout.NoBorders
 import XMonad.Layout.Tabbed
 import XMonad.Util.Run (spawnPipe)
 
-main = xmonad =<< statusBar myBar myPP toggleStrutsKey myConfig
-
 -- Command to launch the bar.
 myBar = "xmobar -x 1"
 
@@ -20,13 +18,31 @@ myPP = xmobarPP { ppCurrent = xmobarColor "#429942" "" . wrap "<" ">" }
 -- Key binding to toggle the gap for the bar.
 toggleStrutsKey XConfig {XMonad.modMask = modMask} = (modMask, xK_b)
 
+myManageHook = composeAll . concat $
+    [ [ className =? c --> doFloat | c <- classFloats ]
+    , [ title     =? t --> doFloat | t <- titleFloats ]
+    , [ isFullscreen --> doFullFloat
+      , isDialog     --> doCenterFloat
+      ]
+    ]
+  where
+    classFloats = ["MPlayer"]
+    titleFloats = []
+
+myLayoutHook = tiled ||| Mirror tiled ||| Full
+    where
+        tiled = Tall nmaster delta ratio
+        nmaster = 1
+        delta = 3/100
+        ratio = 1/2
+
 -- Configuration
 myConfig = defaultConfig
         { terminal = "gnome-terminal"
-        , manageHook = manageDocks <+> manageHook defaultConfig
-        , layoutHook = noBorders $
-            Tall 1 (3/100) (1/2) |||
-            Mirror (Tall 1 (3/100) (1/2)) |||
-            Full
-        , logHook = fadeInactiveLogHook 0.9
+        , manageHook = myManageHook
+        , layoutHook = myLayoutHook
+        , logHook = fadeInactiveLogHook 0.95
+        , borderWidth = 0
         }
+
+main = xmonad =<< statusBar myBar myPP toggleStrutsKey myConfig
