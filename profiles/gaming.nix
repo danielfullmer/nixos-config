b0cefa6ec08fe830d5d838f23c9042342d007d6b
Daniel Fullmer <daniel.fullmer@yale.edu>
2015-09-06 18:20:30 +0000

    Enable joystick support for gaming.

--- profiles/gaming.nix
+++ profiles/gaming.nix
@@ -1,6 +1,8 @@
 { config, pkgs, lib, ... }:
 {
-  hardware.opengl.driSupport32Bit = true;
+  hardware.opengl.driSupport32Bit = true; # Needed for steam
+
+  services.xserver.modules = [ pkgs.xlibs.xf86inputjoystick ];
 
   environment.systemPackages = (with pkgs; [
     steam
