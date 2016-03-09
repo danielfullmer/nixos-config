1418a64b9d202ada232730df8982a6e8ba8b2278
Daniel Fullmer <daniel.fullmer@yale.edu>
2016-03-08 21:48:14 -0500

    steam: Added steam controller support

--- profiles/gaming.nix
+++ profiles/gaming.nix
@@ -6,5 +6,30 @@
 
   environment.systemPackages = (with pkgs; [
     steam
+    steamcontroller-udev-rules
   ]);
+
+  # Steam controller stuff
+  nixpkgs.config.packageOverrides = pkgs: {
+    steamcontroller-udev-rules = pkgs.writeTextFile {
+      name = "steamcontroller-udev-rules";
+      text = ''
+        # This rule is needed for basic functionality of the controller in
+        # Steam and keyboard/mouse emulation
+        SUBSYSTEM=="usb", ATTRS{idVendor}=="28de", MODE="0666"
+
+        # This rule is necessary for gamepad emulation; make sure you
+        # replace 'pgriffais' with the username of the user that runs Steam
+        KERNEL=="uinput", MODE="0660", GROUP="wheel", OPTIONS+="static_node=uinput"
+        # systemd option not yet tested
+        #KERNEL=="uinput", SUBSYSTEM=="misc", TAG+="uaccess", TAG+="udev-acl"
+
+        # HTC Vive HID Sensor naming and permissioning
+        KERNEL=="hidraw*", SUBSYSTEM=="hidraw", ATTRS{idVendor}=="0bb4", ATTRS{idProduct}=="2c87", MODE="0666"
+      '';
+      destination = "/etc/udev/rules.d/99-steamcontroller.rules";
+    };
+  };
+
+  services.udev.packages = [ pkgs.steamcontroller-udev-rules ];
 }
