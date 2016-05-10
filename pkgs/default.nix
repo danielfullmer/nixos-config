2118742ceaa85c4814af91e9100568ed854261ad
Daniel Fullmer <daniel.fullmer@yale.edu>
2016-05-10 18:05:52 -0400

    zerotierone: Fix segfault issue

--- pkgs/default.nix
+++ pkgs/default.nix
@@ -2,4 +2,14 @@
 
 rec {
   base16 = pkgs.callPackage ./base16.nix {};
+
+  # Fix an issue with newer versions of gcc
+  zerotierone = pkgs.lib.overrideDerivation pkgs.zerotierone (attrs: {
+    patches = [
+      (pkgs.fetchurl {
+        url = "https://github.com/zerotier/ZeroTierOne/commit/039790cf267cb67a5130fb82caf97998d8b0959e.patch";
+        sha256 = "1n93gvi3d3jsb84k496rhs61ycq5wih1yn47wiz2jwfd83bryarj";
+      })
+    ];
+  });
 }
