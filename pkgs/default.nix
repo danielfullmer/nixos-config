22f7a3d744f938fbfb0e33dcb562ad323592b235
Daniel Fullmer <daniel.fullmer@yale.edu>
2016-06-30 22:54:19 -0400

    zerotier is fixed in nixpkgs

--- pkgs/default.nix
+++ pkgs/default.nix
@@ -1,13 +1,13 @@
 { pkgs }:
 
 rec {
-  # Fix an issue with newer versions of gcc
-  zerotierone = pkgs.lib.overrideDerivation pkgs.zerotierone (attrs: {
-    patches = [
-      (pkgs.fetchurl {
-        url = "https://github.com/zerotier/ZeroTierOne/commit/039790cf267cb67a5130fb82caf97998d8b0959e.patch";
-        sha256 = "1n93gvi3d3jsb84k496rhs61ycq5wih1yn47wiz2jwfd83bryarj";
-      })
-    ];
-  });
+### Example to patch a derivation
+#  zerotierone = pkgs.lib.overrideDerivation pkgs.zerotierone (attrs: {
+#    patches = [
+#      (pkgs.fetchurl {
+#        url = "https://github.com/zerotier/ZeroTierOne/commit/039790cf267cb67a5130fb82caf97998d8b0959e.patch";
+#        sha256 = "1n93gvi3d3jsb84k496rhs61ycq5wih1yn47wiz2jwfd83bryarj";
+#      })
+#    ];
+#  });
 }
