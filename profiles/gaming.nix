70663a79d95f4c03085497669d623b170118fe05
Daniel Fullmer <daniel.fullmer@yale.edu>
2016-12-20 23:12:11 -0500

    Updated udev rules for steam controller
    Based on http://steamcommunity.com/app/353370/discussions/0/490123197956024380/

--- profiles/gaming.nix
+++ profiles/gaming.nix
@@ -14,15 +14,32 @@
     steamcontroller-udev-rules = pkgs.writeTextFile {
       name = "steamcontroller-udev-rules";
       text = ''
-        # This rule is needed for basic functionality of the controller in
-        # Steam and keyboard/mouse emulation
+        # This rule is needed for basic functionality of the controller in Steam and keyboard/mouse emulation
         SUBSYSTEM=="usb", ATTRS{idVendor}=="28de", MODE="0666"
 
-        # This rule is necessary for gamepad emulation; make sure you
-        # replace 'pgriffais' with the username of the user that runs Steam
+        # This rule is necessary for gamepad emulation; make sure you replace 'pgriffais' with a group that the user that runs Steam belongs to
         KERNEL=="uinput", MODE="0660", GROUP="wheel", OPTIONS+="static_node=uinput"
-        # systemd option not yet tested
-        #KERNEL=="uinput", SUBSYSTEM=="misc", TAG+="uaccess", TAG+="udev-acl"
+
+        # Valve HID devices over USB hidraw
+        KERNEL=="hidraw*", ATTRS{idVendor}=="28de", MODE="0666"
+
+        # Valve HID devices over bluetooth hidraw
+        KERNEL=="hidraw*", KERNELS=="*28DE:*", MODE="0666"
+
+        # DualShock 4 over USB hidraw
+        KERNEL=="hidraw*", ATTRS{idVendor}=="054c", ATTRS{idProduct}=="05c4", MODE="0666"
+
+        # DualShock 4 wireless adapter over USB hidraw
+        KERNEL=="hidraw*", ATTRS{idVendor}=="054c", ATTRS{idProduct}=="0ba0", MODE="0666"
+
+        # DualShock 4 Slim over USB hidraw
+        KERNEL=="hidraw*", ATTRS{idVendor}=="054c", ATTRS{idProduct}=="09cc", MODE="0666"
+
+        # DualShock 4 over bluetooth hidraw
+        KERNEL=="hidraw*", KERNELS=="*054C:05C4*", MODE="0666"
+
+        # DualShock 4 Slim over bluetooth hidraw
+        KERNEL=="hidraw*", KERNELS=="*054C:09CC*", MODE="0666"
 
         # HTC Vive HID Sensor naming and permissioning
         KERNEL=="hidraw*", SUBSYSTEM=="hidraw", ATTRS{idVendor}=="0bb4", ATTRS{idProduct}=="2c87", MODE="0666"
