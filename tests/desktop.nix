760fc0e1754dfa5badeb8acf76d861b3e9c3dd1b
Daniel Fullmer <daniel.fullmer@yale.edu>
2016-12-21 19:03:07 -0500

    Update desktop test with new screenshots

--- tests/desktop.nix
+++ tests/desktop.nix
@@ -13,13 +13,29 @@ import <nixpkgs/nixos/tests/make-test.nix> ({ pkgs, ...} : {
     ];
   };
 
-  testScript =
+  # Use our overrides for neofetch
+  testScript = with pkgs // (import ../pkgs { inherit pkgs; });
     ''
       $machine->waitForX;
+      $machine->waitForFile("/home/danielrf/.Xauthority");
+      $machine->succeed("xauth merge ~danielrf/.Xauthority");
+      $machine->waitForWindow(qr/i3bar/);
       $machine->sleep(5);
-      $machine->sendKeys("alt-ret");
+      $machine->screenshot("startup");
+
+      # Need to explicitly reference the config file
+      $machine->succeed("su - danielrf -s /bin/sh -c 'DISPLAY=:0 ${termite}/bin/termite -t Termite -e \"${neofetch}/bin/neofetch --config ${neofetch}/share/neofetch/config\" --hold &'");
+      $machine->waitForWindow(qr/Termite/);
+      $machine->sleep(10);
+      $machine->screenshot("terminal");
+      $machine->succeed("su - danielrf -s /bin/sh -c 'kill `pgrep termite`'");
+
+      $machine->succeed("su - danielrf -s /bin/sh -c 'DISPLAY=:0 ${awf}/bin/awf-gtk2 &'");
+      $machine->succeed("su - danielrf -s /bin/sh -c 'DISPLAY=:0 ${awf}/bin/awf-gtk3 &'");
+      $machine->waitForWindow(qr/A widget factory.*Gtk2/);
+      $machine->waitForWindow(qr/A widget factory.*Gtk3/);
       $machine->sleep(5);
-      $machine->sendChars("hello\n");
-      $machine->screenshot("screenshot");
+      $machine->screenshot("gtkwidgets");
+      $machine->succeed("su - danielrf -s /bin/sh -c 'kill `pgrep awf-gtk`'");
     '';
 })
