3d81d491fdb1097381377aa921e51355e8048b98
Daniel Fullmer <danielrf12@gmail.com>
2017-01-24 12:40:31 -0500

    desktop: Enable rescuetime

--- profiles/desktop/default.nix
+++ profiles/desktop/default.nix
@@ -90,6 +90,7 @@ with lib;
             (${pkgs.pasystray}/bin/pasystray) &
             (${pkgs.xss-lock}/bin/xss-lock -- ${pkgs.i3lock-fancy}/bin/i3lock-fancy) &
             (${pkgs.dunst}/bin/dunst -conf ${dunstFile}) &
+            (${pkgs.rescuetime}/bin/rescuetime) &
             #(${pkgs.emacs}/bin/emacs --daemon && ${pkgs.emacs}/bin/emacsclient -c) &
 
             ${config.services.xserver.desktopManager.extraSessionCommands}
