b2c4cb144db25012235f1f1ad634ac8ea9eb06d2
Daniel Fullmer <danielrf12@gmail.com>
2017-02-21 21:33:04 -0500

    Add udev rules for HTC Vive

--- profiles/gaming.nix
+++ profiles/gaming.nix
@@ -6,48 +6,29 @@
   services.xserver.modules = [ pkgs.xlibs.xf86inputjoystick ];
 
   environment.systemPackages = (with pkgs; [
-    (steam.override {newStdcpp = true; })
-    steamcontroller-udev-rules
+    (steam.override {newStdcpp = true; }) # Option needed for radeon drivers
   ]);
 
-  # Steam controller stuff
-  nixpkgs.config.packageOverrides = pkgs: {
-    steamcontroller-udev-rules = pkgs.writeTextFile {
-      name = "steamcontroller-udev-rules";
-      text = ''
-        # This rule is needed for basic functionality of the controller in Steam and keyboard/mouse emulation
-        SUBSYSTEM=="usb", ATTRS{idVendor}=="28de", MODE="0666"
+  services.udev.extraRules = ''
+    ### Steam Controller ###
+    SUBSYSTEM=="usb", ATTRS{idVendor}=="28de", TAG+="uaccess"
+    KERNEL=="uinput", MODE="0660", GROUP="users", OPTIONS+="static_node=uinput"
 
-        # This rule is necessary for gamepad emulation; make sure you replace 'pgriffais' with a group that the user that runs Steam belongs to
-        KERNEL=="uinput", MODE="0660", GROUP="wheel", OPTIONS+="static_node=uinput"
+    ### HTC Vive ###
 
-        # Valve HID devices over USB hidraw
-        KERNEL=="hidraw*", ATTRS{idVendor}=="28de", MODE="0666"
-
-        # Valve HID devices over bluetooth hidraw
-        KERNEL=="hidraw*", KERNELS=="*28DE:*", MODE="0666"
-
-        # DualShock 4 over USB hidraw
-        KERNEL=="hidraw*", ATTRS{idVendor}=="054c", ATTRS{idProduct}=="05c4", MODE="0666"
-
-        # DualShock 4 wireless adapter over USB hidraw
-        KERNEL=="hidraw*", ATTRS{idVendor}=="054c", ATTRS{idProduct}=="0ba0", MODE="0666"
-
-        # DualShock 4 Slim over USB hidraw
-        KERNEL=="hidraw*", ATTRS{idVendor}=="054c", ATTRS{idProduct}=="09cc", MODE="0666"
-
-        # DualShock 4 over bluetooth hidraw
-        KERNEL=="hidraw*", KERNELS=="*054C:05C4*", MODE="0666"
-
-        # DualShock 4 Slim over bluetooth hidraw
-        KERNEL=="hidraw*", KERNELS=="*054C:09CC*", MODE="0666"
-
-        # HTC Vive HID Sensor naming and permissioning
-        KERNEL=="hidraw*", SUBSYSTEM=="hidraw", ATTRS{idVendor}=="0bb4", ATTRS{idProduct}=="2c87", MODE="0666"
-      '';
-      destination = "/etc/udev/rules.d/99-steamcontroller.rules";
-    };
-  };
-
-  services.udev.packages = [ pkgs.steamcontroller-udev-rules ];
+    # HTC Vive HID Sensor naming and permissioning
+    KERNEL=="hidraw*", SUBSYSTEM=="hidraw", ATTRS{idVendor}=="0bb4", ATTRS{idProduct}=="2c87", TAG+="uaccess"
+    KERNEL=="hidraw*", SUBSYSTEM=="hidraw", ATTRS{idVendor}=="28de", ATTRS{idProduct}=="2101", TAG+="uaccess"
+    KERNEL=="hidraw*", SUBSYSTEM=="hidraw", ATTRS{idVendor}=="28de", ATTRS{idProduct}=="2000", TAG+="uaccess"
+    KERNEL=="hidraw*", SUBSYSTEM=="hidraw", ATTRS{idVendor}=="28de", ATTRS{idProduct}=="1043", TAG+="uaccess"
+    KERNEL=="hidraw*", SUBSYSTEM=="hidraw", ATTRS{idVendor}=="28de", ATTRS{idProduct}=="2050", TAG+="uaccess"
+    KERNEL=="hidraw*", SUBSYSTEM=="hidraw", ATTRS{idVendor}=="28de", ATTRS{idProduct}=="2011", TAG+="uaccess"
+    KERNEL=="hidraw*", SUBSYSTEM=="hidraw", ATTRS{idVendor}=="28de", ATTRS{idProduct}=="2012", TAG+="uaccess"
+    SUBSYSTEM=="usb", ATTRS{idVendor}=="0bb4", ATTRS{idProduct}=="2c87", TAG+="uaccess"
+    # HTC Camera USB Node
+    SUBSYSTEM=="usb", ATTRS{idVendor}=="114d", ATTRS{idProduct}=="8328", TAG+="uaccess"
+    # HTC Mass Storage Node
+    SUBSYSTEM=="usb", ATTRS{idVendor}=="114d", ATTRS{idProduct}=="8200", TAG+="uaccess"
+    SUBSYSTEM=="usb", ATTRS{idVendor}=="114d", ATTRS{idProduct}=="8a12", TAG+="uaccess"
+  '';
 }
