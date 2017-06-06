03a4ed0de6fa4fe3f49262c2ab276decb0659b4f
Daniel Fullmer <danielrf12@gmail.com>
2017-06-06 16:11:15 -0400

    release.nix: Output a nixpkgs channel

--- release.nix
+++ release.nix
@@ -1,18 +1,18 @@
-let
-  pkgs' = import <nixpkgs> {};
-in
-{ nixpkgs ? pkgs'.fetchFromGitHub {
-    owner = "nixos";
-    repo = "nixpkgs";
-    rev = "0843ad10fd5cf984b36cf7e6533a518cab299af4";
-    sha256 = "1c8jwqszhr0383pbbr11qls2k61akbhwjmj6q2ck4z2c3m6rcsxk";
-  }
+{ nixpkgs ? { outPath = ./../nixpkgs; revCount = 56789; shortRev = "gfedcba"; }
+, stableBranch ? false
+, supportedSystems ? [ "x86_64-linux" ]
 }:
 
-with pkgs'.lib;
-
+# See also: https://github.com/openlab-aux/vuizvui
 let
+  pkgs = import nixpkgs {};
+  lib = import (nixpkgs + /lib);
   nixos = nixpkgs + /nixos;
+
+  version = lib.fileContents (nixos + "/../.version");
+  versionSuffix =
+    (if stableBranch then "." else "pre") + "${toString nixpkgs.revCount}.${nixpkgs.shortRev}";
+
 in
 {
   bellman = (import nixos { configuration = ./machines/bellman.nix; }).system;
@@ -21,5 +21,7 @@ in
   nyquist = (import nixos { configuration = ./machines/nyquist.nix; }).system;
   euler = (import nixos { configuration = ./machines/euler.nix; }).system;
 
-  tests.desktop = hydraJob (import ./tests/desktop.nix {});
+  channel = (import (nixos + "/lib/make-channel.nix") { inherit pkgs nixpkgs version versionSuffix; });
+
+  tests.desktop = lib.hydraJob (import ./tests/desktop.nix {});
 }
