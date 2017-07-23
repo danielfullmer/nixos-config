d2ef6ccce014b6032722fab5dcf7b782c33f0994
Daniel Fullmer <danielrf12@gmail.com>
2017-07-22 21:49:20 -0400

    Add preprint package to latex env

--- profiles/academic.nix
+++ profiles/academic.nix
@@ -2,7 +2,7 @@
 {
   environment.systemPackages = (with pkgs; [
     (texlive.combine {
-      inherit (texlive) scheme-medium filehook exam pgf pgfplots subfigure;
+      inherit (texlive) scheme-medium filehook exam pgf pgfplots subfigure preprint;
 # collection-latexextra
     })
 
