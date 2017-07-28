98c4bb4109353468b96f15fc3f0253db7177f7e0
Daniel Fullmer <danielrf12@gmail.com>
2017-07-27 22:18:03 -0400

    postfix: New config to match module in nixpkgs

--- profiles/postfix.nix
+++ profiles/postfix.nix
@@ -3,17 +3,17 @@
   services.postfix = {
     enable = true;
     # Thanks to http://rs20.mine.nu/w/2011/07/gmail-as-relay-host-in-postfix/
-#    relayHost = "smtp.gmail.com:587";
-    networks = ["127.0.0.0/8" "30.0.0.0/8"];
-    rootAlias = "cgibreak@gmail.com";
-    extraConfig = ''
-      inet_protocols=ipv4
-      relayhost=[smtp.gmail.com]:587
-      smtp_use_tls=yes
-      smtp_tls_CAfile=/etc/ssl/certs/ca-bundle.crt
-      smtp_sasl_auth_enable=yes
-      smtp_sasl_password_maps=hash:/etc/postfix.local/sasl_passwd
-      smtp_sasl_security_options=noanonymous
-    '';
+    # TODO: security options. Cert or password?
+    rootAlias = "cgibreak@gmail.com"; # TODO
+    config = {
+      mynetworks = ["127.0.0.0/8" "30.0.0.0/8"];
+      inet_protocols = "ipv4";
+      relayhost = "[smtp.gmail.com]:587";
+      smtp_use_tls = "yes";
+      smtp_tls_CAfile = "/etc/ssl/certs/ca-bundle.crt";
+      smtp_sasl_auth_enable = "yes";
+      smtp_sasl_password_maps = "hash:/etc/postfix.local/sasl_passwd";
+      smtp_sasl_security_options = "noanonymous";
+    };
   };
 }
