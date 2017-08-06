50d9cab72d38fadde4aaa0f4a0c8e3cbfcaa6f1b
Daniel Fullmer <danielrf12@gmail.com>
2017-08-06 17:09:31 -0400

    Fix uaccess hotplug issues

--- profiles/gaming.nix
+++ profiles/gaming.nix
@@ -9,9 +9,29 @@
     (steam.override {newStdcpp = true; }) # Option needed for radeon drivers
   ]);
 
-  services.udev.extraRules = ''
-    ### Steam Controller ###
-    SUBSYSTEM=="usb", ATTRS{idVendor}=="28de", TAG+="uaccess"
-    KERNEL=="uinput", MODE="0660", GROUP="users", OPTIONS+="static_node=uinput"
-  '';
+  services.udev.packages = [
+    (pkgs.writeTextFile {
+      name = "steamvr-udev-rules";
+      destination = "/etc/udev/rules.d/60-steamvr.rules"; # Need to have a number before 73-seat-late.rules where uaccess is actually activated
+      text = ''
+        # HTC Vive HID Sensor naming and permissioning
+        KERNEL=="hidraw*", SUBSYSTEM=="hidraw", ATTRS{idVendor}=="0bb4", ATTRS{idProduct}=="2c87", TAG+="uaccess"
+        KERNEL=="hidraw*", SUBSYSTEM=="hidraw", ATTRS{idVendor}=="28de", ATTRS{idProduct}=="2101", TAG+="uaccess"
+        KERNEL=="hidraw*", SUBSYSTEM=="hidraw", ATTRS{idVendor}=="28de", ATTRS{idProduct}=="2000", TAG+="uaccess"
+        KERNEL=="hidraw*", SUBSYSTEM=="hidraw", ATTRS{idVendor}=="28de", ATTRS{idProduct}=="1043", TAG+="uaccess"
+        KERNEL=="hidraw*", SUBSYSTEM=="hidraw", ATTRS{idVendor}=="28de", ATTRS{idProduct}=="2050", TAG+="uaccess"
+        KERNEL=="hidraw*", SUBSYSTEM=="hidraw", ATTRS{idVendor}=="28de", ATTRS{idProduct}=="2011", TAG+="uaccess"
+        KERNEL=="hidraw*", SUBSYSTEM=="hidraw", ATTRS{idVendor}=="28de", ATTRS{idProduct}=="2012", TAG+="uaccess"
+        SUBSYSTEM=="usb", ATTRS{idVendor}=="0bb4", ATTRS{idProduct}=="2c87", TAG+="uaccess"
+        # HTC Camera USB Node
+        SUBSYSTEM=="usb", ATTRS{idVendor}=="114d", ATTRS{idProduct}=="8328", TAG+="uaccess"
+        # HTC Mass Storage Node
+        SUBSYSTEM=="usb", ATTRS{idVendor}=="114d", ATTRS{idProduct}=="8200", TAG+="uaccess"
+        SUBSYSTEM=="usb", ATTRS{idVendor}=="114d", ATTRS{idProduct}=="8a12", TAG+="uaccess"
+
+        # Steam Controller
+        KERNEL=="hidraw*", SUBSYSTEM=="hidraw", ATTRS{idVendor}=="28de", ATTRS{idProduct}=="1142", TAG+="uaccess"
+      '';
+    })
+  ];
 }
--- profiles/yubikey.nix
+++ profiles/yubikey.nix
@@ -1,15 +1,7 @@
 { config, pkgs, lib, ... }:
 {
-  #services.udev.packages = [ pkgs.libu2f-host ];
-  services.udev.extraRules = ''
-    # From https://github.com/Yubico/libu2f-host/blob/master/70-u2f.rules
-    ACTION!="add|change", GOTO="u2f_end"
-
-    # Yubico YubiKey
-    KERNEL=="hidraw*", SUBSYSTEM=="hidraw", ATTRS{idVendor}=="1050", ATTRS{idProduct}=="0113|0114|0115|0116|0120|0402|0403|0406|0407|0410", TAG+="uaccess"
-
-    LABEL="u2f_end"
-  '';
+  # udev rules
+  services.udev.packages = with pkgs; [ libu2f-host yubikey-personalization ];
 
   # For smartcards
   services.pcscd.enable = true;
