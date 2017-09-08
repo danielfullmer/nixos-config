800397c246c3933b9fc009aa4bdd08baa4fbdd7d
Daniel Fullmer <danielrf12@gmail.com>
2017-09-08 09:45:20 -0400

    gmailieer: 0.2 -> 0.3

--- pkgs/gmailieer/default.nix
+++ pkgs/gmailieer/default.nix
@@ -2,13 +2,13 @@
 
 python3Packages.buildPythonApplication rec {
   name = "gmailieer-${version}";
-  version = "0.2";
+  version = "0.3";
 
   src = fetchFromGitHub {
     owner = "gauteh";
     repo = "gmailieer";
     rev = "v${version}";
-    sha256 = "0ix9j6rb2iss8h6kh46p6ar409r6z3jjlssfh82vgyb51svfvawf";
+    sha256 = "1app783gf0p9p196nqsgbyl6s1bp304dfav86fqiq86h1scld787";
   };
 
   propagatedBuildInputs = with python3Packages; [ tqdm google_api_python_client oauth2client notmuch ];
