def24f8ca7b9a3b8adf6993abe679f2a195d9ecd
Daniel Fullmer <danielrf12@gmail.com>
2017-09-11 15:32:51 -0400

    nyquist: Add LUKS support

--- machines/nyquist.nix
+++ machines/nyquist.nix
@@ -18,9 +18,23 @@
   boot.kernelModules = [ ];
   boot.extraModulePackages = [ ];
 
+  boot.initrd.luks.devices = {
+    nyquist-root = {
+      device = "/dev/disk/by-uuid/98d7c646-dc01-4683-bde8-eba6cb301ab9";
+      preLVM = true;
+      allowDiscards = true;
+    };
+  };
+
   fileSystems."/" =
     { device = "/dev/disk/by-uuid/584a34cb-f494-48d6-b308-eb4ae7d37c84";
       fsType = "ext4";
+      options = [ "discard" ];
+    };
+
+  fileSystems."/boot" =
+    { device = "/dev/disk/by-uuid/C22D-1A1F";
+      fsType = "vfat";
     };
 
   fileSystems."/drf36" = {
