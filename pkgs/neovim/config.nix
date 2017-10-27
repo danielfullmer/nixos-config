56b2b8e08b36e2a2765af1baf2b995896b6e246e
Daniel Fullmer <danielrf12@gmail.com>
2017-10-26 21:47:32 -0400

    neovim: Remove jedi options--included in nvim-completion-manager

--- pkgs/neovim/config.nix
+++ pkgs/neovim/config.nix
@@ -235,12 +235,6 @@ let g:pymode_run = 0
 let g:pymode_lint = 0
 let g:pymode_rope = 0
 
-autocmd FileType python setlocal omnifunc=jedi#completions
-let g:jedi#auto_vim_configuration = 0
-let g:jedi#popup_select_first = 0
-let g:jedi#popup_on_dot = 0
-let g:jedi#completions_enable = 0
-
 let g:haddock_browser = "xdg-open"
 let g:pandoc_syntax_dont_use_conceal_for_rules = ['atx', 'titleblock']
 map <leader>vp <Esc>:VimuxPromptCommand<CR>
