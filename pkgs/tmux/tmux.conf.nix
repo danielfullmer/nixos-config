879246a5f69f11b1383803262a1378bda97d4ab7
Daniel Fullmer <danielrf12@gmail.com>
2017-10-29 18:40:50 -0400

    neovim: Fix tmux-navigation

--- pkgs/tmux/tmux.conf.nix
+++ pkgs/tmux/tmux.conf.nix
@@ -42,12 +42,17 @@ bind -n M-v split-window -h
 # Smart pane switching with awareness of Vim splits.
 # See: https://github.com/christoomey/vim-tmux-navigator
 is_vim="ps -o state= -o comm= -t '#{pane_tty}' \
-    | grep -iqE \'^[^TXZ ]+ +(\\S+\\/)?g?(view|n?vim?x?)(diff)?$\'"
+    | grep -iqE '^[^TXZ ]+ +(\\S+\\/)?g?(view|\.?n?vim?x?)(-wrapped)?(diff)?$'"
 bind-key -n M-h if-shell "$is_vim" "send-keys C-h"  "select-pane -L"
 bind-key -n M-j if-shell "$is_vim" "send-keys C-j"  "select-pane -D"
 bind-key -n M-k if-shell "$is_vim" "send-keys C-k"  "select-pane -U"
 bind-key -n M-l if-shell "$is_vim" "send-keys C-l"  "select-pane -R"
 bind-key -n M-\ if-shell "$is_vim" "send-keys C-\\" "select-pane -l"
+bind-key -T copy-mode-vi M-h select-pane -L
+bind-key -T copy-mode-vi M-j select-pane -D
+bind-key -T copy-mode-vi M-k select-pane -U
+bind-key -T copy-mode-vi M-l select-pane -R
+bind-key -T copy-mode-vi M-\ select-pane -l
 
 bind -rn M-H resize-pane -L 3
 bind -rn M-J resize-pane -R 3
