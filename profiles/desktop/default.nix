1cd056f75221a74cec1dee724f079f287b5be5a4
Daniel Fullmer <danielrf12@gmail.com>
2018-03-10 17:55:07 -0500

    desktop: Set background properly

--- profiles/desktop/default.nix
+++ profiles/desktop/default.nix
@@ -38,6 +38,7 @@ with lib;
           (${pkgs.networkmanagerapplet}/bin/nm-applet) &
           (${pkgs.pasystray}/bin/pasystray) &
           (${pkgs.xss-lock}/bin/xss-lock -- ${pkgs.i3lock-fancy}/bin/i3lock-fancy) &
+          (${pkgs.feh}/bin/feh --bg-scale ${config.theme.background}) &
           (${pkgs.dunst}/bin/dunst -conf ${dunstFile}) &
           (${pkgs.ibus}/bin/ibus-daemon -d) &
           #(${pkgs.emacs}/bin/emacs --daemon && ${pkgs.emacs}/bin/emacsclient -c) &
