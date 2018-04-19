2270b251cf8ec8716e728239d433f44e51fc8e37
Daniel Fullmer <danielrf12@gmail.com>
2018-04-18 21:55:15 -0400

    Add backup nameserver

--- profiles/base.nix
+++ profiles/base.nix
@@ -74,7 +74,7 @@ in
       })))
     '';
   };
-  networking.nameservers = [ "127.0.0.1" "::1" ];
+  networking.nameservers = [ "127.0.0.1" "::1" "8.8.8.8" ];
 
   nix = {
     nixPath = [ # From nixos/modules/services/misc/nix-daemon.nix
