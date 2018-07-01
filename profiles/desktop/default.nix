df10ad6680793997cc497e31d3e41c7de503fa3e
Daniel Fullmer <danielrf12@gmail.com>
2018-06-30 23:20:36 -0400

    desktop: Disable remote pulseaudio stuff

--- profiles/desktop/default.nix
+++ profiles/desktop/default.nix
@@ -81,10 +81,10 @@ with lib;
 
   hardware.pulseaudio = {
     enable = true;
-    tcp.enable = true;
-    tcp.anonymousClients.allowedIpRanges = [ "30.0.0.0/24" ];
-    zeroconf.discovery.enable = true;
-    zeroconf.publish.enable = true;
+#    tcp.enable = true;
+#    tcp.anonymousClients.allowedIpRanges = [ "30.0.0.0/24" ];
+#    zeroconf.discovery.enable = true;
+#    zeroconf.publish.enable = true;
   };
 
   security.rtkit.enable = true;
