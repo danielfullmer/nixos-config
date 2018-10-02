7953aa905a06a97cee4a454caf1727401535a216
Daniel Fullmer <danielrf12@gmail.com>
2018-10-01 20:28:21 -0400

    steam: Use udev rules from steam package

--- profiles/gaming.nix
+++ profiles/gaming.nix
@@ -2,6 +2,7 @@
 {
   hardware.opengl.driSupport32Bit = true; # Needed for steam
   hardware.pulseaudio.support32Bit = true;
+  hardware.steam-hardware.enable = true; # Provides udev rules for controller and HTC vive
 
   services.xserver.modules = [ pkgs.xlibs.xf86inputjoystick ];
 
@@ -28,31 +29,4 @@
     item = "nofile";
     value = "1048576";
   }];
-
-
-  services.udev.packages = [
-    (pkgs.writeTextFile {
-      name = "steamvr-udev-rules";
-      destination = "/etc/udev/rules.d/60-steamvr.rules"; # Need to have a number before 73-seat-late.rules where uaccess is actually activated
-      text = ''
-        # HTC Vive HID Sensor naming and permissioning
-        KERNEL=="hidraw*", SUBSYSTEM=="hidraw", ATTRS{idVendor}=="0bb4", ATTRS{idProduct}=="2c87", TAG+="uaccess"
-        KERNEL=="hidraw*", SUBSYSTEM=="hidraw", ATTRS{idVendor}=="28de", ATTRS{idProduct}=="2101", TAG+="uaccess"
-        KERNEL=="hidraw*", SUBSYSTEM=="hidraw", ATTRS{idVendor}=="28de", ATTRS{idProduct}=="2000", TAG+="uaccess"
-        KERNEL=="hidraw*", SUBSYSTEM=="hidraw", ATTRS{idVendor}=="28de", ATTRS{idProduct}=="1043", TAG+="uaccess"
-        KERNEL=="hidraw*", SUBSYSTEM=="hidraw", ATTRS{idVendor}=="28de", ATTRS{idProduct}=="2050", TAG+="uaccess"
-        KERNEL=="hidraw*", SUBSYSTEM=="hidraw", ATTRS{idVendor}=="28de", ATTRS{idProduct}=="2011", TAG+="uaccess"
-        KERNEL=="hidraw*", SUBSYSTEM=="hidraw", ATTRS{idVendor}=="28de", ATTRS{idProduct}=="2012", TAG+="uaccess"
-        SUBSYSTEM=="usb", ATTRS{idVendor}=="0bb4", ATTRS{idProduct}=="2c87", TAG+="uaccess"
-        # HTC Camera USB Node
-        SUBSYSTEM=="usb", ATTRS{idVendor}=="114d", ATTRS{idProduct}=="8328", TAG+="uaccess"
-        # HTC Mass Storage Node
-        SUBSYSTEM=="usb", ATTRS{idVendor}=="114d", ATTRS{idProduct}=="8200", TAG+="uaccess"
-        SUBSYSTEM=="usb", ATTRS{idVendor}=="114d", ATTRS{idProduct}=="8a12", TAG+="uaccess"
-
-        # Steam Controller
-        KERNEL=="hidraw*", SUBSYSTEM=="hidraw", ATTRS{idVendor}=="28de", ATTRS{idProduct}=="1142", TAG+="uaccess"
-      '';
-    })
-  ];
 }
