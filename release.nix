af42e3db900b391b475aa229d086154215bf4a59
Daniel Fullmer <danielrf12@gmail.com>
2018-12-01 22:55:22 -0500

    Add test for building and displaying w/ latexmk

--- release.nix
+++ release.nix
@@ -20,12 +20,13 @@ rec {
   tests.desktop = lib.hydraJob (import ./tests/desktop.nix {});
   tests.gpg-agent = lib.hydraJob (import ./tests/gpg-agent.nix {});
   tests.gpg-agent-x11 = lib.hydraJob (import ./tests/gpg-agent-x11.nix {});
+  tests.latex-pdf = lib.hydraJob (import ./tests/latex-pdf.nix {});
 
   tested = pkgs.releaseTools.aggregate {
     name = "tested";
     constituents = [
       bellman bellman-vfio nyquist euler spaceheater
-      tests.desktop tests.gpg-agent tests.gpg-agent-x11
+      tests.desktop tests.gpg-agent tests.gpg-agent-x11 tests.latex-pdf
     ];
   };
 
---
+++ tests/latex-pdf.nix
@@ -0,0 +1,31 @@
+import <nixpkgs/nixos/tests/make-test.nix> ({ pkgs, ...} : {
+  name = "latex-pdf";
+
+  machine = { config, pkgs, ... }: {
+    imports = [
+      ../profiles/base.nix
+      ../profiles/desktop/default.nix
+      ../profiles/academic.nix
+      ../profiles/autologin.nix
+    ];
+    virtualisation.memorySize = 1024;
+  };
+
+  enableOCR = true;
+
+  testScript =
+    ''
+      $machine->waitForX;
+      $machine->waitForFile("/home/danielrf/.Xauthority");
+      $machine->succeed("xauth merge ~danielrf/.Xauthority");
+      $machine->waitForWindow(qr/i3bar/);
+      $machine->sleep(5);
+
+      $machine->succeed("su - danielrf -s /bin/sh -c 'cp ${./latex-pdf.tex} /home/danielrf/latex-pdf.tex'");
+      $machine->succeed("su - danielrf -s /bin/sh -c 'latexmk /home/danielrf/latex-pdf.tex &'");
+      $machine->waitForWindow(qr/zathura/);
+      $machine->sleep(5);
+      $machine->screenshot("zathura");
+      $machine->waitForText("Title");
+    '';
+})
