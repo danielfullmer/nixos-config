cf5b4bd6f7df01e906f8bfd2becd24f65c377e38
Daniel Fullmer <danielrf12@gmail.com>
2019-02-13 19:50:15 -0500

    st: New config file change from upstream

--- pkgs/st/config.h.nix
+++ pkgs/st/config.h.nix
@@ -143,8 +143,8 @@ static Shortcut shortcuts[] = {
 	{ TERMMOD,              XK_C,           clipcopy,       {.i =  0} },
 	{ TERMMOD,              XK_V,           clippaste,      {.i =  0} },
 	{ TERMMOD,              XK_Y,           selpaste,       {.i =  0} },
+	{ ShiftMask,            XK_Insert,      selpaste,       {.i =  0} },
 	{ TERMMOD,              XK_Num_Lock,    numlock,        {.i =  0} },
-	{ TERMMOD,              XK_I,           iso14755,       {.i =  0} },
 };
 
 /*
