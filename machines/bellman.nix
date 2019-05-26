3f0296a808638b02ce3ba6ad47b51c6c0c1cb729
Daniel Fullmer <danielrf12@gmail.com>
2019-05-25 23:21:59 -0400

    bellman: Use a /tmp zfs with sync=disabled

--- machines/bellman.nix
+++ machines/bellman.nix
@@ -46,6 +46,7 @@
     "/" = { device = "pool/root"; fsType = "zfs"; };
     "/home" = { device = "pool/home"; fsType = "zfs"; };
     "/nix" = { device = "pool/nix"; fsType = "zfs"; };
+    "/tmp" = { device = "pool/tmp"; fsType = "zfs"; };
 
     "/boot" = {
       device = "/dev/disk/by-uuid/3AF1-2802";
