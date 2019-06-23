80c2ba18aba7afa5998d271e24726675394fcb97
Daniel Fullmer <danielrf12@gmail.com>
2019-06-23 18:06:10 -0400

    Open firewall ports for KDEConnect

--- profiles/extended.nix
+++ profiles/extended.nix
@@ -51,5 +51,9 @@
     ${pkgs.kdeconnect}/bin/kdeconnect-indicator &
   '';
 
+  # KDE Connect
+  networking.firewall.allowedTCPPortRanges = [ { from=1714; to=1764; } ];
+  networking.firewall.allowedUDPPortRanges = [ { from=1714; to=1764; } ];
+
   environment.etc."chromium/native-messaging-hosts/org.kde.plasma.browser_integration.json".source = "${pkgs.plasma-browser-integration}/etc/chromium/native-messaging-hosts/org.kde.plasma.browser_integration.json";
 }
