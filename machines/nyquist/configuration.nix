a48d01b60a47a0cd79e6c12af6946c0ce38c877b
Daniel Fullmer <danielrf12@gmail.com>
2020-01-03 13:27:54 -0500

    nyquist: Set up camera

--- machines/nyquist/configuration.nix
+++ machines/nyquist/configuration.nix
@@ -24,4 +24,28 @@
 
   programs.adb.enable = true;
   users.users.danielrf.extraGroups = [ "adbusers" ];
+
+#  services.zoneminder = {
+#    enable = true;
+#    database = {
+#      createLocally = true;
+#      username = "zoneminder";
+#    };
+#    hostname = "zoneminder.daniel.fullmer.me";
+#  };
+
+  systemd.services."camera-office" = {
+    wantedBy = [ "multi-user.target" ];
+    requires = [ "network-online.target" ];
+    # require nginx?
+    script = ''
+      ${pkgs.ffmpeg_4.bin}/bin/ffmpeg -nostats -f video4linux2 -input_format mjpeg -video_size 640x480 -framerate 10 -i /dev/video0 -vcodec copy -f mjpeg "tcp://0.0.0.0:9000?listen"
+    '';
+    serviceConfig = {
+      DynamicUser = true;
+      Group = "video";
+      Restart = "always";
+      RestartSec = 15;
+    };
+  };
 }
