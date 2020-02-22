cf1a541613a22757e84cd6e2f7708ce104951fce
Daniel Fullmer <danielrf12@gmail.com>
2020-02-21 19:07:57 -0500

    steam: some comments

--- profiles/gaming.nix
+++ profiles/gaming.nix
@@ -13,9 +13,17 @@
 
   nixpkgs.overlays = [ (self: super: {
     steam = super.steam.override (
-      { #nativeOnly = true;
+      { #nativeOnly = true; # Otherwise the qt libraries below try to pull in steam runtime libs (which are too old): https://github.com/NixOS/nixpkgs/issues/32881#issuecomment-426513878
         # Extra deps are for steamvr support
-        extraPkgs = p: with p; [ usbutils lsb-release procps dbus_daemon ];
+        extraPkgs = p: with p; [
+          lsb-release dbus_daemon
+          # Libraries needed by vrmonitor. While there is an "extraLibraries"
+          # argument, it puts things in multiPkgs instead of targetPkgs.
+          # multiPkgs makes a copy for each support arch (here 32 and 64-bit),
+          # but we only care about 64-bit and don't want to recompile qtbase
+          #qt5.qtbase qt5.qtmultimedia
+        ];
       });
   }) ];
+  nixpkgs.config.allowBroken = true;
 }
