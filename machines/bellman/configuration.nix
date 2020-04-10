208c02925d8f51fb0438e8ecfcef70b3393a31dd
Daniel Fullmer <danielrf12@gmail.com>
2020-04-10 17:29:00 -0400

    bellman: use hydra-unstable

--- machines/bellman/configuration.nix
+++ machines/bellman/configuration.nix
@@ -122,7 +122,7 @@ with (import ../../profiles/nginxCommon.nix);
     extraConfig = "binary_cache_secret_key_file = /var/secrets/bellman-nix-key.sec";
 
     # Patch to allow builtins.fetchTarball
-    package = pkgs.hydra.overrideAttrs (attrs: { patches = (if attrs ? patches then attrs.patches else []) ++ [ ../../pkgs/hydra/no-restrict-eval.patch ]; });
+    package = pkgs.hydra-unstable.overrideAttrs (attrs: { patches = (if attrs ? patches then attrs.patches else []) ++ [ ../../pkgs/hydra/no-restrict-eval.patch ]; });
   };
   secrets."hydra-nix-key.sec" = { user = "hydra"; group = "hydra"; };
   services.nginx.virtualHosts."hydra.daniel.fullmer.me" = {
