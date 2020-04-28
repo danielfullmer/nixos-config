5a5ffebc38546cc72734d69c474a9d6371f8dc92
Daniel Fullmer <danielrf12@gmail.com>
2020-04-28 00:39:49 -0400

    bellman: fix hostapd regulatory issue

--- machines/bellman/ap.nix
+++ machines/bellman/ap.nix
@@ -1,44 +1,77 @@
 { config, pkgs, ... }:
 let
-  #interface = "wlp71s0f3u4";
   interface = "wlo2";
 in
 {
-#  networking.bridges.br0.interfaces = [ interface "enp68s0" ];
-#  networking.interfaces."${interface}".proxyARP = true;
-#  networking.interfaces.enp68s0.proxyARP = true;
+  # Intel Wifi 6 AX200 with hostapd has an issue where it doesn't try starting
+  # an AP since it thinks the channels are not allowed in the regulatory
+  # domain. The device seems to manage its own regdb (visible with "iw reg get")
+  # Before starting, it is in the "country 00" state with a bunch of channels
+  # having IR restrictions, preventing hostapd from trying to bring up the AP.
+  # Notably, when running "iw dev wlo2 scan", this would change to "country US"
+  # with permissive regulatory restrictions. However, running hostapd
+  # immediately after the iw scan was insufficient, as it would go back to
+  # "country 00". One ugly workaround was to run networkmanager simultaneously
+  # with hostapd, but networkmanager would clobber other settings I wanted to
+  # set for the interface. The workaround I settled on was to just have hostapd
+  # ignore it's own channel checks and try to bring up the AP anyway. This
+  # seems to work, and it switches to "country US" when doing so.
+  nixpkgs.overlays = [ (self: super: {
+    hostapd = super.hostapd.overrideAttrs ({ patches ? [], ... }: {
+      patches = patches ++ [ ./hostapd-doitanyway.patch ];
+    });
+  }) ];
   services.hostapd = {
     enable = true;
     inherit interface;
     ssid = "controlnet2_nomap";
     hwMode = "a"; # Just means 5ghz
+    #channel = 0; # ACS. Seems to be needed for the card to set the right regulatory location
     channel = 36;
     # https://wiki.gentoo.org/wiki/Hostapd
     # https://medium.com/@renaudcerrato/how-to-build-your-own-wireless-router-from-scratch-part-3-d54eecce157f
     # https://blog.fraggod.net/2017/04/27/wifi-hostapd-configuration-for-80211ac-networks.html
     # Set up for 80MHz. Pixel 3 claims to get 780MBps on this
     extraConfig = ''
-      ieee80211n=1
-      ieee80211ac=1
-      #require_ht=1
-      #require_vht=1
       wmm_enabled=1
       rsn_pairwise=CCMP
       wpa_key_mgmt=WPA-PSK WPA-PSK-SHA256
       country_code=US
+
+      ieee80211d=1
+      ieee80211h=1
+
+      ieee80211n=1
+      #require_ht=1
       ht_capab=[HT40+][SHORT-GI-20][SHORT-GI-40][DSSS_CCK-40][MAX-AMSDU-7935][TX-STBC][RX-STBC][LDPC]
-      vht_oper_chwidth=1
-      vht_oper_centr_freq_seg0_idx=42
+      obss_interval=1
 
-
-      # Intel wifi 6 AX200
+      ieee80211ac=1
+      #require_vht=1
       vht_capab=[RXLDPC][SHORT-GI-80][SHORT-GI-160][TX-STBC-2BY1][SU-BEAMFORMEE][MU-BEAMFORMEE]
+
+      #ieee80211ax=1
+      #he_su_beamformer=1
+      #he_mu_beamformer=1
+
+      vht_oper_chwidth=1
+      #vht_oper_chwidth=2
+      vht_oper_centr_freq_seg0_idx=42
+      #vht_oper_centr_freq_seg1_idx=159
+
+      acs_num_scans=1
     '';
     # (v)ht_capab from comparing output of "iw list" with example hostapd.conf
     # For 8812au
     #vht_capab=[SHORT-GI-80][TX-STBC-2BY1][SU-BEAMFORMEE][HTC-VHT]
     wpaPassphrase = "verysecure2";
   };
+  networking.interfaces."${interface}".ipv4.addresses = [ { address = "192.168.4.1"; prefixLength = 24; }];
+  networking.firewall.trustedInterfaces = [ interface ];
+  networking.nat.enable = true;
+  networking.nat.externalInterface = "enp68s0";
+  networking.nat.internalInterfaces = [ interface ];
+
 #  services.dnsmasq = {
 #    enable = true;
 #    resolveLocalQueries = false;
@@ -49,11 +82,11 @@ in
 #      dhcp-range=192.168.4.2,192.168.4.254
 #    '';
 #  };
-  networking.interfaces."${interface}".ipv4.addresses = [ { address = "192.168.4.1"; prefixLength = 24; }];
-  networking.firewall.trustedInterfaces = [ interface ];
-  networking.nat.enable = true;
-  networking.nat.externalInterface = "enp68s0";
-  networking.nat.internalInterfaces = [ interface ];
+
+#  networking.bridges.br0.interfaces = [ interface "enp68s0" ];
+#  networking.interfaces."${interface}".proxyARP = true;
+#  networking.interfaces.enp68s0.proxyARP = true;
 
   environment.systemPackages = with pkgs; [ iw wirelesstools ];
+  services.udev.packages = with pkgs; [ crda ];
 }
