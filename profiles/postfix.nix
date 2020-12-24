3c5baf8a761a328f01ef86795bee1271b9cceca2
Daniel Fullmer <danielrf12@gmail.com>
2020-12-23 19:25:24 -0800

    postfix: add multi-account configuration

--- profiles/postfix.nix
+++ profiles/postfix.nix
@@ -8,10 +8,11 @@
     config = {
       mynetworks = ["127.0.0.0/8" "30.0.0.0/8"];
       inet_protocols = "ipv4";
-      relayhost = "[smtp.gmail.com]:587";
+      sender_dependent_relayhost_maps = "hash:/etc/postfix.local/relay_maps";
       smtp_use_tls = "yes";
       smtp_sasl_auth_enable = "yes";
       smtp_sasl_password_maps = "hash:/etc/postfix.local/sasl_passwd";
+      smtp_sender_dependent_authentication = "yes";
       smtp_sasl_security_options = "noanonymous";
     };
   };
