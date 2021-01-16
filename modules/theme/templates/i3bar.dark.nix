19e496186b6ac76f2a911ad49f067ef1347ad8fe
Daniel Fullmer <danielrf12@gmail.com>
2021-01-15 21:03:07 -0800

    desktop: reduce fill in i3bar

--- modules/theme/templates/i3bar.dark.nix
+++ modules/theme/templates/i3bar.dark.nix
@@ -5,7 +5,7 @@ with colors; ''
         separator $base03
         background $base00
         statusline $base05
-        focused_workspace $base0C $base0D $base00
+        focused_workspace $base0C $base00 $base0C
         active_workspace $base02 $base02 $base07
         inactive_workspace $base01 $base01 $base03
         urgent_workspace $base08 $base08 $base07
