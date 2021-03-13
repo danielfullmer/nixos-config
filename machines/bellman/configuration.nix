bebc578a706a18471cabb89e3519a4eae7ca4d73
Daniel Fullmer <danielrf12@gmail.com>
2021-03-12 19:49:27 -0800

    bellman: Put netbooting devices on their own vlan

--- machines/bellman/configuration.nix
+++ machines/bellman/configuration.nix
@@ -42,6 +42,11 @@ with (import ../../profiles/nginxCommon.nix);
   networking.interfaces.enp68s0.ipv4.addresses = [ { address = "192.168.1.200"; prefixLength = 24; } ];
   networking.defaultGateway = "192.168.1.1";
 
+  networking.vlans.netboot = {
+    id = 3;
+    interface = "enp68s0";
+  };
+
   # Router ipv6 isn't working. Lets tunnel through tunnelbroker.net. Notably helps zerotier connections as well
   # TODO: Add a periodic client IP udpate
   networking.localCommands = ''
--- profiles/pxe.nix
+++ profiles/pxe.nix
@@ -2,22 +2,22 @@
 # Try out https://github.com/NixOS/nixpkgs/pull/83443
 let
   #interface = "enp69s0";
-  interface = "enp68s0";
+  interface = "netboot"; # On a VLAN
   subnetNumber = 5;
   networkPrefix = "192.168.${toString subnetNumber}";
+  ipAddress = "${networkPrefix}.1";
 in
 {
-  networking.interfaces.${interface}.ipv4.addresses = [ { address = "${networkPrefix}.1"; prefixLength = 24; } ];
+  networking.interfaces.${interface}.ipv4.addresses = [ { address = ipAddress; prefixLength = 24; } ];
   networking.firewall.trustedInterfaces = [ interface ]; # TODO. Manually set ports to be exported
   networking.nat.internalInterfaces = [ interface ];
 
   services.nfs.server = {
     enable = true;
-    hostName = "192.168.1.200"; # Listen on this address
+    hostName = ipAddress; # Listen on this address
     exports = ''
-      /nix/store 192.168.1.0/16(ro)
+      /nix/store ${networkPrefix}.0/24(ro)
     '';
-      #/nix/store ${networkPrefix}.0/24(ro)
   };
 
   services.dnsmasq = {
@@ -27,8 +27,7 @@ in
 
       # First MAC address is for pinecube uboot, native linux has a different
       # MAC and will use normal DHCP
-      dhcp-host=02:01:51:db:b2:9d,set:pxe
-      dhcp-range=interface:${interface},tag:pxe,${networkPrefix}.2,${networkPrefix}.254
+      dhcp-range=interface:${interface},${networkPrefix}.2,${networkPrefix}.254
       enable-tftp
       tftp-root=/var/lib/tftpboot
     '';
