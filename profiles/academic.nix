8e16f1e2d7824270854117e8a5f47b9ad4234272
Daniel Fullmer <danielrf12@gmail.com>
2021-09-02 21:44:40 -0700

    Update academic profile

--- profiles/academic.nix
+++ profiles/academic.nix
@@ -3,10 +3,17 @@
   environment.systemPackages = (with pkgs; [
     (texlive.combine {
       inherit (texlive) scheme-medium filehook exam pgf pgfplots subfigure preprint;
-# collection-latexextra
+      #collection-latexrecommended
     })
 
     git-latexdiff
     proselint
+
+    zotero # mendeley alternative
+    papis # CLI mendeley alternative
+    rmapi # Remarkable API
   ]);
+
+  # Zotero connector extension
+  programs.chromium.extensions = [ "ekhagklcjbdpajgpjgmbionohlpdbjgc" ];
 }
