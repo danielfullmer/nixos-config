e9ffbd5fd5c42c3f59b72c4714f47c6170640bdd
Daniel Fullmer <danielrf12@gmail.com>
2021-12-15 12:32:08 -0800

    riemann: enable HW accelerated video decoding

--- machines/riemann/configuration.nix
+++ machines/riemann/configuration.nix
@@ -35,4 +35,24 @@
   services.xserver.libinput.enable = true;
 
   theme.fontSize = 8;
+
+  # HW Accelerated video decoding
+  nixpkgs.config.packageOverrides = pkgs: {
+    vaapiIntel = pkgs.vaapiIntel.override { enableHybridCodec = true; };
+  };
+  hardware.opengl = {
+    enable = true;
+    extraPackages = with pkgs; [
+      intel-media-driver # LIBVA_DRIVER_NAME=iHD
+      vaapiIntel         # LIBVA_DRIVER_NAME=i965 (older but works better for Firefox/Chromium)
+      vaapiVdpau
+      libvdpau-va-gl
+    ];
+  };
+
+  environment.variables = {
+    LIBVA_DRIVER_NAME="iHD";
+  };
+
+  nixpkgs.config.chromium.commandLineArgs = "--enable-features=VaapiVideoDecoder --use-gl=egl";
 }
