0969830d7a4a4ea325197f466c52c2405651af52
Daniel Fullmer <danielrf12@gmail.com>
2021-12-15 20:13:30 -0800

    riemann: allow broken for zfs

--- machines/riemann/configuration.nix
+++ machines/riemann/configuration.nix
@@ -26,6 +26,7 @@
   programs.light.enable = true;
 
   boot.kernelPackages = pkgs.linuxPackages_latest; # For wifi support
+  nixpkgs.config.allowBroken = true; # To allow ZFS with latest kernel
   boot.kernelParams = [ "mem_sleep_default=deep" ];
   services.fprintd.enable = true; # Fingerprint support
   services.fwupd.enable = true; # Firmware updates
