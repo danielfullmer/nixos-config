e2240cb73e78191f4cec62c91cb5ee79493ca937
Daniel Fullmer <danielrf12@gmail.com>
2022-01-07 00:53:04 -0800

    kdeconnect: only listen on controlnet

--- profiles/kdeconnect.nix
+++ profiles/kdeconnect.nix
@@ -11,8 +11,10 @@
   };
 
   # KDE Connect
-  networking.firewall.allowedTCPPortRanges = [ { from=1714; to=1764; } ];
-  networking.firewall.allowedUDPPortRanges = [ { from=1714; to=1764; } ];
+  networking.firewall.interfaces."${config.controlnet.ap.interface}" = {
+    allowedTCPPortRanges = [ { from=1714; to=1764; } ];
+    allowedUDPPortRanges = [ { from=1714; to=1764; } ];
+  };
 
   environment.etc."chromium/native-messaging-hosts/org.kde.plasma.browser_integration.json".source = "${pkgs.plasma-browser-integration}/etc/chromium/native-messaging-hosts/org.kde.plasma.browser_integration.json";
 }
