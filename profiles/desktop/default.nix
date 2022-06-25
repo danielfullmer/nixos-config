31d56cae0f102e009fe4d39c1042b7abd4278216
Daniel Fullmer <danielrf12@gmail.com>
2022-06-25 15:33:57 -0700

    Switch to using pipewire instead of pulseaudio

--- profiles/desktop/default.nix
+++ profiles/desktop/default.nix
@@ -127,14 +127,23 @@ with lib;
     };
   };
 
-  hardware.pulseaudio = {
+  services.pipewire = {
     enable = true;
-#    tcp.enable = true;
-#    tcp.anonymousClients.allowedIpRanges = [ "30.0.0.0/24" ];
-#    zeroconf.discovery.enable = true;
-#    zeroconf.publish.enable = true;
+    alsa.enable = true;
+    alsa.support32Bit = true;
+    pulse.enable = true;
+    wireplumber.enable = true;
   };
-
+  environment.etc = {
+    "wireplumber/bluetooth.lua.d/51-bluez-config.lua".text = ''
+      bluez_monitor.properties = {
+        ["bluez5.enable-sbc-xq"] = true,
+        ["bluez5.enable-msbc"] = true,
+        ["bluez5.enable-hw-volume"] = true,
+        ["bluez5.headset-roles"] = "[ hsp_hs hsp_ag hfp_hf hfp_ag ]"
+      }
+    '';
+  };
   security.rtkit.enable = true;
 
   environment.systemPackages = (with pkgs; [
