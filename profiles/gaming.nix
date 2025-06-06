8192818e3ee880a8a75c55385ce3e6a249b43601
Daniel Fullmer <danielrf12@gmail.com>
2025-06-05 17:29:58 -0700

    Update gaming profile

--- profiles/gaming.nix
+++ profiles/gaming.nix
@@ -4,13 +4,13 @@
   hardware.steam-hardware.enable = true; # Provides udev rules for controller, HTC vive, and Valve Index
 
   hardware.graphics.enable32Bit = true; # Needed for steam
-  hardware.opengl.driSupport32Bit = true; # Needed for steam
   hardware.pulseaudio.support32Bit = true;
 
   services.xserver.modules = [ pkgs.xorg.xf86inputjoystick ];
 
   environment.systemPackages = with pkgs; [
     gamescope
+    mangohud
   ];
 
 #  nixpkgs.overlays = [ (self: super: {
